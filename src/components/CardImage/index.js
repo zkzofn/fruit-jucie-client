bffbd7ebefe19274558990748818226f12070095
Jangho Lee <zkzofn@gmail.com>
2017-12-15 15:17:41 +0900

    Edit Card

--- src/components/CardImage/index.js
+++ src/components/CardImage/index.js
@@ -6,25 +6,32 @@ import FlatButton from 'material-ui/FlatButton';
 /**
  * @props
  *    height
+ *    width
+ *    imgUrl
  */
 export default class CardImage extends Component {
   constructor(props) {
     super(props)
+
+    this.imgUrl = props.imgUrl ? props.imgUrl : "https://i.imgur.com/tG6Sw83.jpg";
   }
 
   render() {
     const styles = {
       card: {
         height: this.props.height ? this.props.height : 200,
+        width: this.props.width ? this.props.width: 200
       }
-    }
+    };
+
+
 
     return (
       <Card style={styles.card}>
         <CardMedia
           overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
         >
-          <img src="https://i.imgur.com/tG6Sw83.jpg" alt="" />
+          <img src={this.imgUrl} alt="" />
         </CardMedia>
       </Card>
     )
