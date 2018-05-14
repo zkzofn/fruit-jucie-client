b42b1043c9b86be39df82af32f095d554c7447a7
zkzofn <zkzofn@gmail.com>
2018-05-15 00:10:17 +0900

    Edit port number

--- webpack.config.js
+++ webpack.config.js
@@ -51,7 +51,7 @@ module.exports = {
   devServer: {
     historyApiFallback: true,
     contentBase: './',
-    port: 8000
+    port: 8080
     // proxy: {
     //   "/api": {
     //     target: "http://localhost:3000",
